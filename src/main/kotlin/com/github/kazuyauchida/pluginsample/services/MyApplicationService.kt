2c6e1a7c795d54736ce1070275059f79ea6036d9
GitHub Action <action@github.com>
2023-02-10 01:24:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/kazuyauchida/pluginsample/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kazuyauchida/pluginsample/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kazuyauchida.pluginsample.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kazuyauchida.pluginsample.MyBundle
 
 class MyApplicationService {
 
